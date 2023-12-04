2f689cc340ca863ddc3c660fa108815d687b6c9b
Yerofey S. <pm@yerofey.dev>
2023-12-04 17:40:11 +0100

    Update ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,18 @@
-module.exports = {
-  apps : [{
-    name   : "the-ship",
-    script : "./index.js",
+export const apps = [
+  {
+    name: 'the-ship',
+    script: './index.js',
     instances: 1,
     time: true,
     watch: true,
-    ignore_watch : ["node_modules", ".git", ".gitignore", "ecosystem.config.js", "package.json", "package-lock.json", "README.md"],
-  }]
-}
+    ignore_watch: [
+      'node_modules',
+      '.git',
+      '.gitignore',
+      'ecosystem.config.js',
+      'package.json',
+      'package-lock.json',
+      'README.md',
+    ],
+  },
+];
